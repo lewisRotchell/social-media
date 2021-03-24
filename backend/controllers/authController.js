7c6d5c3fe077dc45a5464392e5930ead36762128
Lewis Rotchell <rotchell92@gmail.com>
2021-03-24 16:13:42 +0000

    commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,15 @@ import catchAsync from "../utils/catchAsync.js";
 import User from "../models/userModel.js";
 
 const register = catchAsync(async (req, res, next) => {
+  const { username, email, password, photo, role } = req.body;
+
+  const user = await User.create({
+    username,
+    email,
+    password,
+    photo,
+    role,
+  });
   res.status(200).json({
     msg: "success",
   });
