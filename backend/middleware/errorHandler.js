889361679b2d67fc9d14663db64aeaf8a5333438
Lewis Rotchell <rotchell92@gmail.com>
2021-03-25 10:26:16 +0000

    added error handling

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,8 +1,17 @@
 import AppError from "../utils/AppError.js";
 
+const handleValidtionErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode === 200 ? 500 : err.statusCode;
-  res.status(statusCode).json({
+  // let statusCode = err.statusCode === 200 ? 500 : err.statusCode;
+  if (err.name === "ValidationError") err = handleValidtionErrorDB(err);
+
+  console.log(err.errors);
+  res.status(err.statusCode || 500).json({
     success: false,
     message: err.message,
     stack: process.env.NODE_ENV == "production" ? null : err.stack,
