2d3b478cb5f77ad9004211996fc3a1ffbd197359
Lewis Rotchell <rotchell92@gmail.com>
2021-03-25 17:06:01 +0000

    Added postfeed backend

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -20,7 +20,7 @@ const createPost = catchAsync(async (req, res, next) => {
 const getPosts = catchAsync(async (req, res, next) => {
   const posts = await Post.find().populate({
     path: "user",
-    select: "_id, photo, username",
+    select: { _id: 1, username: 1, photo: 1 },
   });
 
   res.status(200).json({
@@ -29,4 +29,23 @@ const getPosts = catchAsync(async (req, res, next) => {
   });
 });
 
-export { createPost, getPosts };
+const getFollowingPosts = catchAsync(async (req, res, next) => {
+  const user = req.user;
+  //Add user posts to post list to show user's own posts
+  user.following.push(user._id);
+  console.log(user);
+
+  const posts = await Post.find({ user: { $in: user.following } })
+    .populate({
+      path: "user",
+      select: { _id: 1, username: 1, photo: 1 },
+    })
+    .sort("-createdAt");
+
+  res.status(200).json({
+    status: "success",
+    posts,
+  });
+});
+
+export { createPost, getPosts, getFollowingPosts };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/postController.js";
+import {
+  createPost,
+  getFollowingPosts,
+  getPosts,
+} from "../controllers/postController.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, createPost).get(protect, getPosts);
+router.route("/newsfeed").get(protect, getFollowingPosts);
 
 export default router;
